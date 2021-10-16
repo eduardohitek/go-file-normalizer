673b63593df132d64f67250f8c1704c96bd45ff0
Eduardo Hitek <eduardohitek@gmail.com>
2021-10-16 12:58:55 -0300

    feat: adicionando cmd

---
+++ cmd/root.go
@@ -0,0 +1,17 @@
+package cmd
+
+import (
+	"github.com/eduardohitek/go-file-normalizer/cmd/rename"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "file-rename",
+	Short: "File rename",
+	Long:  "",
+}
+
+func init() {
+
+	rootCmd.AddCommand(rename.RenameCmd)
+}
